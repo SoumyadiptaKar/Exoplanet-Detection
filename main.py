34b58d060afcb9982a37fcccabbd084c05c8198b
Soumyadipta Kar <k.sammy2002@gmail.com>
2024-03-14 22:01:29 +0530

    Stage 02: Prepare Base Model Complete

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from exoplanetDetection.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from exoplanetDetection.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from exoplanetDetection import logger
 
 
@@ -6,6 +7,18 @@ STAGE_NAME = "Data Ingestion stage"
 try:
     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare Base Model stage"
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    data_ingestion = PrepareBaseModelTrainingPipeline()
     data_ingestion.main()
     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<")
 
